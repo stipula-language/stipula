1d06f0c21c232e3908402ac0d245bd4f5e6dda75
adeleveschetti <adele.veschetti@gmail.com>
2022-07-01 12:40:54 +0200

    project updated

--- Stipula-LAN/src/ast/NewInterpreter.java
+++
@@ -1,7 +0,0 @@
-package ast;
-
-public class NewInterpreter {
-
-	
-	
-}
--- Stipula-LAN/src/ast/StipulaTypeInference.java
+++
@@ -1,9 +0,0 @@
-package ast;
-
-public class StipulaTypeInference {
-
-	public static Node assignTypeNames() {
-		return null;
-	}
-	
-}
